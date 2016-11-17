3dfa53075991cde6cc16e30761aa7e0d532f7ca3
hongan94 <phamhongan2201@gmail.com>
2016-11-17 11:56:59 +0700

    layout-header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'homes#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -53,4 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :homes
 end
