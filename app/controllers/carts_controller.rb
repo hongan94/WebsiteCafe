2fa8dc7acce8b9fc77d8aad0818401fbeae8c197
hongan94 <phamhongan2201@gmail.com>
2016-11-28 18:51:40 +0700

    edit-delete-cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,11 +2,34 @@ class CartsController < ApplicationController
   layout :false
   def add
     @product = Product.find(params[:id])
-    @quantity = params[:quantity]
+    @quantity = params[:quantity].to_i
     @cart.add(@product, @product.price, @quantity)
+    redirect_to homes_path
+  end
 
+  def update
+    @item = CartItem.find(params[:id])
+    if params[:quantity].to_i.zero?
+      @item.destroy
+    else
+      @item.update_quantity(params[:quantity])
+    end
+    redirect_to cart_path(session[:cart_id]), notice: 'You was update successfully!'
+  end
+
+  def destroy
+    if params[:id].present?
+      @item = CartItem.find(params[:id])
+      @item.destroy
+    else
+       @item  = CartItem.where(owner_id: session[:cart_id])
+       @item.destroy_all
+    end
+      flash[:notice] = "You has remove item successfully!"
+      redirect_to cart_path(session[:cart_id])
   end
 
   def show
+    @item  = CartItem.where(owner_id: session[:cart_id]).order(id: :desc)
   end
 end
